163b4057d13412cedde76b4d788077fba2945ff7
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2023-08-13 00:48:17 -0400

    Added multilabel chunking functionality

--- src/hipool/curiam_reader.py
+++ src/hipool/curiam_reader.py
@@ -9,9 +9,9 @@ where t is the number of tokens in the document.
 """
 
 import json
+import math
 
 from hipool.curiam_categories import ORDERED_CATEGORIES
-
 
 import torch
 
@@ -29,10 +29,15 @@ class CuriamDataset(Dataset):
     https://github.com/mkranzlein/curiam/blob/main/corpus/corpus.json
     """
 
-    def __init__(self, json_file):
-        data = self.read_json(json_file)
-        self.documents = data["documents"]
-        self.labels = data["labels"]
+    def __init__(self, json_file, tokenizer, max_len, chunk_len, overlap_len):
+        _ = self.read_json(json_file)
+        self.documents = _["documents"]
+        self.labels = _["labels"]
+        self.num_classes = 9
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.chunk_len = chunk_len
+        self.overlap_len = overlap_len
 
     def read_json(self, json_file: str) -> dict:
         with open(json_file, "r", encoding="utf-8") as f:
@@ -62,8 +67,71 @@ class CuriamDataset(Dataset):
             labels.append(torch.stack(document_labels))
         return {"documents": documents, "labels": labels}
 
+    def shuffle(self, seed) -> None:
+        raise NotImplementedError
+
     def __len__(self) -> int:
         return len(self.documents)
 
+    def chunk_document(self, document, targets):
+        document = " ".join(document)
+
+        batch_encoding = self.tokenizer.encode_plus(
+            document,
+            max_length=self.max_len,
+            pad_to_max_length=False,
+            add_special_tokens=True,
+            return_attention_mask=True,
+            return_token_type_ids=True,
+            return_overflowing_tokens=True,
+            return_tensors='pt')
+
+        input_ids_list = []
+        attention_mask_list = []
+        token_type_ids_list = []
+        targets_list = []
+
+        previous_input_ids = batch_encoding["input_ids"].squeeze()
+
+        start_token = torch.tensor([101], dtype=torch.long)
+        end_token = torch.tensor([102], dtype=torch.long)
+
+        total_token = len(previous_input_ids) - 2  # Exclude head 101, tail 102
+        stride = self.overlap_len - 2
+        number_chunks = math.floor(total_token/stride)
+
+        mask_list = torch.ones(self.chunk_len, dtype=torch.long)
+        type_list = torch.zeros(self.chunk_len, dtype=torch.long)
+        zeroed_labels = torch.zeros(self.num_classes)
+        for current in range(number_chunks-1):
+            input_ids = previous_input_ids[current*stride:current*stride+self.chunk_len-2]
+            chunk_targets = targets[current*stride:current*stride+self.chunk_len-2,:]
+            input_ids = torch.cat((start_token, input_ids, end_token))
+            # MK: in eval, make sure to only eval each token once and don't include start and end toks
+            chunk_targets = torch.cat((zeroed_labels.unsqueeze(dim=0), chunk_targets, zeroed_labels.unsqueeze(dim=0)))
+            input_ids_list.append(input_ids)
+
+            attention_mask_list.append(mask_list)
+            token_type_ids_list.append(type_list)
+            targets_list.append(chunk_targets)
+
+        if len(input_ids_list) == 0:
+            raise ValueError("input_ids_list cannot have length 0")
+
+        return ({
+            'ids': input_ids_list,
+            'mask': attention_mask_list,
+            'token_type_ids': token_type_ids_list,
+            'targets': targets_list,
+            'len': [torch.tensor(len(targets_list), dtype=torch.long)]
+        })
+
     def __getitem__(self, idx):
-        return self.documents[idx], self.labels[idx]
+        """  Return a single tokenized sample at a given positon [idx] from data"""
+
+        document = self.documents[idx]
+        targets = self.labels[idx]
+
+        chunked_document = self.chunk_document(document, targets)
+
+        return chunked_document
