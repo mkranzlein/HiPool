77e2caa61537560327ce9032b19b4c3ccc5c3aac
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2023-08-12 23:13:27 -0400

    Working dataset reader

---
+++ src/hipool/curiam_categories.py
@@ -0,0 +1,11 @@
+ORDERED_CATEGORIES = [
+    "Focal Term",
+    "Definition",
+    "Metalinguistic Cue",
+    "Direct Quote",
+    "Legal Source",
+    "Language Source",
+    "Named Interpretive Rule",
+    "Example Use",
+    "Appeal to Meaning",
+]
--- src/hipool/curiam_reader.py
+++ src/hipool/curiam_reader.py
@@ -10,36 +10,60 @@ where t is the number of tokens in the document.
 
 import json
 
+from hipool.curiam_categories import ORDERED_CATEGORIES
+
+
+import torch
+
 from torch.utils.data import Dataset
 
-
-with open("data/curiam_sample.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-ORDERED_CATEGORIES = [
-    "Focal Term",
-    "Definition",
-    "Metalinguistic Cue",
-    "Direct Quote",
-    "Legal Source",
-    "Language Source",
-    "Named Interpretive Rule",
-    "Example Use",
-    "Appeal to Meaning",
-]
 
 categories_to_ids = {}
 for i, category in enumerate(ORDERED_CATEGORIES):
     categories_to_ids[category] = i
 
-documents = []
 
-for document in data:
-    document_text = []
-    document_labels = []
-    for sentence in document["sentences"]:
-        for token in sentence["tokens"]:
-            token_labels = []
-            if "annotations" in token:
-                for annotation in token["annotations"]:
-                    token_labels.append(annotation["category"])
+class CuriamDataset(Dataset):
+    """Reads a file formatted like CuRIAM's corpus.json.
+
+    https://github.com/mkranzlein/curiam/blob/main/corpus/corpus.json
+    """
+
+    def __init__(self, json_file):
+        data = self.read_json(json_file)
+        self.documents = data["documents"]
+        self.labels = data["labels"]
+
+    def read_json(self, json_file: str) -> dict:
+        with open(json_file, "r", encoding="utf-8") as f:
+            raw_data = json.load(f)
+
+        documents = []
+        labels = []
+
+        for document in raw_data:
+            document_text = []
+            document_labels = []
+            for sentence in document["sentences"]:
+                for token in sentence["tokens"]:
+                    token_category_ids = []
+                    if "annotations" in token:
+                        for annotation in token["annotations"]:
+                            annotation_category = annotation["category"]
+                            category_id = categories_to_ids[annotation_category]
+                            token_category_ids.append(category_id)
+                    # Binary multilabels
+                    token_labels = torch.zeros(size=(9,))
+                    token_labels[token_category_ids] = 1
+
+                    document_text.append(token["text"])
+                    document_labels.append(token_labels)
+            documents.append(document_text)
+            labels.append(torch.stack(document_labels))
+        return {"documents": documents, "labels": labels}
+
+    def __len__(self) -> int:
+        return len(self.documents)
+
+    def __getitem__(self, idx):
+        return self.documents[idx], self.labels[idx]
