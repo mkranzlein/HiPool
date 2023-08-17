f4304b301cf5aa8c73bdb737e786f926027f9740
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2023-08-17 02:18:23 -0400

    Relocated chunking functionality

--- src/hipool/imdb_reader.py
+++ src/hipool/imdb_reader.py
@@ -7,6 +7,8 @@ import torch
 from sklearn.preprocessing import LabelEncoder
 from torch.utils.data import Dataset
 
+from hipool.chunk import chunk_document
+
 
 class IMDBDataset(Dataset):
     def __init__(self, file_path, tokenizer, max_len, chunk_len,
@@ -14,7 +16,7 @@ class IMDBDataset(Dataset):
         processed_data = self.read_data(file_path)
         self.documents = processed_data["documents"]
         self.labels = processed_data["labels"]
-        self.num_class = 2
+        self.num_labels = 1
         self.tokenizer = tokenizer
         self.max_len = max_len
         self.chunk_len = chunk_len
@@ -30,13 +32,13 @@ class IMDBDataset(Dataset):
         # Turns "positive" and "negative" text labels into 1 and 0
         df["label"] = LE.fit_transform(df["label"])
         df['text'] = df.text.apply(self.clean_txt)
-        documents = list[df["text"]]
-        labels = list(df["labels"])
+        documents = list(df["text"])
+        labels = torch.tensor(df["label"])
         return {"documents": documents, "labels": labels}
 
     def clean_txt(self, text):
         """ Removes special characters from text.
-        
+
         Not sure how necessary this is for this dataset.
         """
 
@@ -93,13 +95,15 @@ class IMDBDataset(Dataset):
     def __getitem__(self, idx):
         """  Return a single tokenized sample at a given positon [idx] from data"""
 
-
         document = self.documents[idx]
-        targets = self.labels[idx]
+        labels = self.labels[idx].unsqueeze(0)
 
-        chunked_document = self.chunk_document(document, targets)
+        chunked_document = chunk_document(document, labels,
+                                          num_labels=1, token_level=False,
+                                          chunk_len=20, overlap_len=10,
+                                          tokenizer=self.tokenizer)
         return chunked_document
 
     def __len__(self):
         """ Return data length """
-        return self.label.shape[0]
+        return len(self.documents)
