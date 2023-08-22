e6f0cfa234f4bb0b8b128f962d62d0bd852a62a5
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2023-08-21 22:25:15 -0400

    Simplified chunking logic and added wordpiece

--- src/hipool/chunk.py
+++ src/hipool/chunk.py
@@ -1,79 +1,55 @@
 """Chunking functionality described Section 2 of the HiPool paper.
 
 https://aclanthology.org/2023.acl-short.16/
+
+k: number of chunks in document
+c: chunk length (in tokens)
+
 """
 
-import math
+from itertools import chain
 
 import torch
-from transformers import BertTokenizer
+from jaxtyping import Float, Integer, jaxtyped
+from torch import Tensor
+from typeguard import typechecked
 
 
-def chunk_document(document: str, labels: torch.Tensor, num_labels: int,
-                   token_level: bool, chunk_len: int, overlap_len: int,
-                   tokenizer: BertTokenizer) -> dict:
-    """Tokenizes a document then splits into chunks of tokens.
+@jaxtyped
+@typechecked
+def chunk_document(input_ids: list[int], first_subword_mask, chunk_len: int, overlap_len: int) -> dict:
+    """Splits a document into chunks of wordpiece subwords."""
 
-    Args:
-        num_labels: 1 for simple binary classification. > 1 for multilabel
-          classification. Each label is assumed to be binary.
-        token_level: If true, each token has a label. Otherwise, each chunk gets
-          a label. In the evaluation script, only the predicted label for the
-          first chunk is evaluated, functionally making chunk-level labeling
-          sequence-level labeling.
-    """
+    chunks = {"input_ids": [],
+              "first_subword_mask": []}
 
-    batch_encoding = tokenizer.encode_plus(
-        document, pad_to_max_length=False, add_special_tokens=True,
-        return_attention_mask=True, return_token_type_ids=True,
-        return_tensors="pt")
-
-    input_ids_list = []
-    attention_mask_list = []
-    token_type_ids_list = []
-    processed_labels = []
-
-    doc_input_ids = batch_encoding["input_ids"].squeeze()
-
-    start_token = torch.tensor([101], dtype=torch.long)
-    end_token = torch.tensor([102], dtype=torch.long)
-
-    num_tokens = len(doc_input_ids) - 2  # Exclude head 101, tail 102
-    stride = overlap_len - 2
-    num_chunks = math.floor(num_tokens / stride)
-
-    # For token-level classification, each token needs a label.
-    # For sequence-level classification, each chunk needs a label.
-
-    mask_list = torch.ones(chunk_len, dtype=torch.long)
-    type_list = torch.zeros(chunk_len, dtype=torch.long)
-    # TODO: Last chunk dimensions can throw off collation function
-    for chunk_id in range(num_chunks - 1):
-        chunk_input_ids = doc_input_ids[0:18]
-        chunk_input_ids = doc_input_ids[chunk_id * stride:chunk_id * stride + chunk_len - 2]
-        chunk_input_ids = torch.cat((start_token, chunk_input_ids, end_token))
-        if token_level:
-            zeroed_labels = torch.zeros(num_labels)
-            chunk_labels = labels[chunk_id * stride:chunk_id * stride + chunk_len - 2, :]
-            # Add 0-labels for start and end tokens
-            chunk_labels = torch.cat((zeroed_labels.unsqueeze(dim=0), chunk_labels, zeroed_labels.unsqueeze(dim=0)))
+    current_idx = 0
+    while True:
+        chunks["input_ids"].append(torch.tensor(input_ids[current_idx: current_idx + chunk_len],
+                                                dtype=torch.long))
+        if current_idx == 0:
+            chunks["first_subword_mask"].append(first_subword_mask[current_idx: current_idx + chunk_len])
         else:
-            # Each chunk gets a label instead of each token.
-            chunk_labels = labels
+            chunks["first_subword_mask"].append(first_subword_mask[current_idx + overlap_len: current_idx + chunk_len])
 
-        # MK: in eval, make sure to only eval each token once and don't include start and end toks
-        input_ids_list.append(chunk_input_ids)
-        attention_mask_list.append(mask_list)
-        token_type_ids_list.append(type_list)
-        processed_labels.append(chunk_labels)
+        if current_idx + chunk_len >= len(input_ids):
+            break
+        else:
+            current_idx += chunk_len - overlap_len
 
-    if len(input_ids_list) == 0:
-        raise ValueError("input_ids_list cannot have length 0")
+    print(sum([len(x) for x in chunks["first_subword_mask"]]))
+    last_chunk_len = chunks["input_ids"][-1].shape[0]
+    last_chunk_ids_padding = torch.zeros((chunk_len - last_chunk_len), dtype=torch.long)  # 0 is pad token
+    chunks["input_ids"][-1] = torch.cat((chunks["input_ids"][-1], last_chunk_ids_padding))
 
-    return ({
-        "ids": input_ids_list,
-        "mask": attention_mask_list,
-        "token_type_ids": token_type_ids_list,
-        "targets": processed_labels,
-        "len": [torch.tensor(len(processed_labels), dtype=torch.long)]
-    })
+    chunks["input_ids"] = torch.stack(chunks["input_ids"])
+    num_chunks = chunks["input_ids"].shape[0]
+    cls_tokens = torch.tensor(101, dtype=torch.long).repeat(num_chunks).unsqueeze(dim=1)
+    sep_tokens = torch.tensor(102, dtype=torch.long).repeat(num_chunks).unsqueeze(dim=1)
+    # Jaxtyping not enforcing dictionary assignments
+    chunks["input_ids"]: Integer[Tensor, "k c"] = torch.cat((cls_tokens, chunks["input_ids"], sep_tokens), dim=1)
+    chunks["attention_mask"] = chunks["input_ids"].not_equal(0).long()
+    chunks["token_type_ids"]: Float[Tensor, "k c"] = torch.zeros(chunks["input_ids"].shape, dtype=torch.long)
+
+    chunks["first_subword_mask"] = list(chain(*chunks["first_subword_mask"]))
+    return chunks
