91f85182fd396524cf5cac747a9d236a70204f2e
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2023-08-21 22:26:33 -0400

    Changed dim name to k

--- src/hipool/models.py
+++ src/hipool/models.py
@@ -1,4 +1,10 @@
-"""Sequence- and Token-level classification models."""
+"""Sequence- and Token-level classification models.
+
+b: batch_size
+k: longest sequence length (in number of chunks)
+c: chunk length (in tokens)
+
+"""
 
 import networkx as nx
 import transformers
@@ -47,10 +53,6 @@ class SequenceClassificationModel(nn.Module):
                 token_type_ids: list[Integer[Tensor, "_ c"]]):
         """Forward pass through the HiPool model.
 
-        b: batch_size
-        s: longest sequence length (in number of chunks)
-        c: chunk length (in tokens)
-
         The three input lists of tensors get padded to the length of the longest
         list of token IDs.
 
@@ -64,22 +66,22 @@ class SequenceClassificationModel(nn.Module):
         # Pad such that each sequence has the same number of chunks
         # Padding chunks c-dim vectors, where all the input ids are 0, which is
         # BERT's padding token
-        padded_ids: Integer[Tensor, "s b c"] = pad_sequence(ids)
-        padded_ids: Integer[Tensor, "b s c"] = padded_ids.permute(1, 0, 2).to(self.device)
-        padded_masks: Integer[Tensor, "s b c"] = pad_sequence(mask)
-        padded_masks: Integer[Tensor, "b s c"] = padded_masks.permute(1, 0, 2).to(self.device)
-        padded_token_type_ids: Integer[Tensor, "s b c"] = pad_sequence(token_type_ids)
-        padded_token_type_ids: Integer[Tensor, "b s c"] = padded_token_type_ids.permute(1, 0, 2).to(self.device)
+        padded_ids: Integer[Tensor, "k b c"] = pad_sequence(ids)
+        padded_ids: Integer[Tensor, "b k c"] = padded_ids.permute(1, 0, 2).to(self.device)
+        padded_masks: Integer[Tensor, "k b c"] = pad_sequence(mask)
+        padded_masks: Integer[Tensor, "b k c"] = padded_masks.permute(1, 0, 2).to(self.device)
+        padded_token_type_ids: Integer[Tensor, "k b c"] = pad_sequence(token_type_ids)
+        padded_token_type_ids: Integer[Tensor, "b k c"] = padded_token_type_ids.permute(1, 0, 2).to(self.device)
         batch_chunk_embeddings = []
         for ids, mask, token_type_ids in zip(padded_ids, padded_masks, padded_token_type_ids):
             results = self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids)
             # One 768-dim embedding for each chunk
-            pooler_output: Float[Tensor, "s 768"] = results["pooler_output"]
+            pooler_output: Float[Tensor, "k 768"] = results["pooler_output"]
             batch_chunk_embeddings.append(pooler_output)
 
-        batch_chunk_embeddings: Float[Tensor, "b s 768"] = torch.stack(batch_chunk_embeddings, 0)
+        batch_chunk_embeddings: Float[Tensor, "b k 768"] = torch.stack(batch_chunk_embeddings, 0)
 
-        linear_layer_output: Float[Tensor, "b s 128"] = self.linear(batch_chunk_embeddings)
+        linear_layer_output: Float[Tensor, "b k 128"] = self.linear(batch_chunk_embeddings)
 
         num_nodes = linear_layer_output.shape[1]
         graph = nx.path_graph(num_nodes)
@@ -112,11 +114,11 @@ class TokenClassificationModel(nn.Module):
 
     """
 
-    def __init__(self, args, num_labels, device, pooling_method='mean'):
+    def __init__(self, args, num_labels, chunk_len, device, pooling_method='mean'):
         super().__init__()
         self.args = args
         self.bert = transformers.BertModel.from_pretrained("bert-base-uncased")
-
+        self.chunk_len = chunk_len + 2
         self.lstm_hidden_size = 128
         self.hidden_dim = 32
 
@@ -133,9 +135,9 @@ class TokenClassificationModel(nn.Module):
 
     @jaxtyped
     @typechecked
-    def forward(self, ids: list[Integer[Tensor, "_ c"]],
-                mask: list[Integer[Tensor, "_ c"]],
-                token_type_ids: list[Integer[Tensor, "_ c"]]):
+    def forward(self, ids: list[Integer[Tensor, "_ d"]],
+                mask: list[Integer[Tensor, "_ d"]],
+                token_type_ids: list[Integer[Tensor, "_ d"]]):
         """Forward pass through the HiPool model.
 
         b: batch_size
@@ -155,25 +157,25 @@ class TokenClassificationModel(nn.Module):
         # Pad such that each sequence has the same number of chunks
         # Padding chunks c-dim vectors, where all the input ids are 0, which is
         # BERT's padding token
-        padded_ids: Integer[Tensor, "s b c"] = pad_sequence(ids)
-        padded_ids: Integer[Tensor, "b s c"] = padded_ids.permute(1, 0, 2).to(self.device)
-        padded_masks: Integer[Tensor, "s b c"] = pad_sequence(mask)
-        padded_masks: Integer[Tensor, "b s c"] = padded_masks.permute(1, 0, 2).to(self.device)
-        padded_token_type_ids: Integer[Tensor, "s b c"] = pad_sequence(token_type_ids)
-        padded_token_type_ids: Integer[Tensor, "b s c"] = padded_token_type_ids.permute(1, 0, 2).to(self.device)
+        padded_ids: Integer[Tensor, "k b c"] = pad_sequence(ids)
+        padded_ids: Integer[Tensor, "b k c"] = padded_ids.permute(1, 0, 2).to(self.device)
+        padded_masks: Integer[Tensor, "k b c"] = pad_sequence(mask)
+        padded_masks: Integer[Tensor, "b k c"] = padded_masks.permute(1, 0, 2).to(self.device)
+        padded_token_type_ids: Integer[Tensor, "k b c"] = pad_sequence(token_type_ids)
+        padded_token_type_ids: Integer[Tensor, "b k c"] = padded_token_type_ids.permute(1, 0, 2).to(self.device)
         batch_chunk_embeddings = []
         batch_token_embeddings = []
         for ids, mask, token_type_ids in zip(padded_ids, padded_masks, padded_token_type_ids):
             results = self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids)
             # One 768-dim embedding for each chunk
-            pooler_output: Float[Tensor, "s 768"] = results["pooler_output"]
-            last_hidden_state: Float[Tensor, "s c 768"] = results["last_hidden_state"]
+            pooler_output: Float[Tensor, "k 768"] = results["pooler_output"]
+            last_hidden_state: Float[Tensor, "k c 768"] = results["last_hidden_state"]
             batch_token_embeddings.append(last_hidden_state)
             batch_chunk_embeddings.append(pooler_output)
 
-        batch_chunk_embeddings: Float[Tensor, "b s 768"] = torch.stack(batch_chunk_embeddings, 0)
+        batch_chunk_embeddings: Float[Tensor, "b k 768"] = torch.stack(batch_chunk_embeddings, 0)
 
-        linear_layer_output: Float[Tensor, "b s 128"] = self.linear(batch_chunk_embeddings)
+        linear_layer_output: Float[Tensor, "b k 128"] = self.linear(batch_chunk_embeddings)
 
         num_nodes = linear_layer_output.shape[1]
         graph = nx.path_graph(num_nodes)
@@ -187,20 +189,20 @@ class TokenClassificationModel(nn.Module):
             gcn_output_batch.append(gcn_output)
         gcn_output_batch: Float[Tensor, "b gcn"] = torch.stack(gcn_output_batch)
 
-        batch_token_embeddings: Float[Tensor, "b s c 768"] = torch.stack(batch_token_embeddings, 0)
+        batch_token_embeddings: Float[Tensor, "b k c 768"] = torch.stack(batch_token_embeddings, 0)
 
         batch_model_output = []
         for sequence_token_embedding, sequence_gcn_output in zip(batch_token_embeddings, gcn_output_batch):
             sequence_outputs = []
             sequence_gcn_output = sequence_gcn_output.unsqueeze(0)
-            repeated_sequence_gcn_output: Float[Tensor, "c gcn"] = sequence_gcn_output.repeat(20, 1)
+            repeated_sequence_gcn_output: Float[Tensor, "c gcn"] = sequence_gcn_output.repeat(self.chunk_len, 1)
             for chunk_token_embedding in sequence_token_embedding:
                 combined_embedding: Float[Tensor, "c 768+gcn"] = torch.cat((chunk_token_embedding,
                                                                             repeated_sequence_gcn_output), dim=1)
                 l2_output: Float[Tensor, "c num_labels"] = self.linear2(combined_embedding)
                 l2_output = nn.functional.sigmoid(l2_output)
                 sequence_outputs.append(l2_output)
-            sequence_outputs: Float[Tensor, "s c num_labels"] = torch.stack(sequence_outputs)
+            sequence_outputs: Float[Tensor, "k c num_labels"] = torch.stack(sequence_outputs)
             batch_model_output.append(sequence_outputs)
         batch_model_output = torch.stack(batch_model_output)
         return batch_model_output
