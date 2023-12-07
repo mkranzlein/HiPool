af0e89238f32fd3f1357f3cb9f8a040339b0b3c0
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2023-12-07 11:24:40 -0500

    Removed obsolete test

--- tests/curiam_reader_test.py
+++ tests/curiam_reader_test.py
@@ -1,5 +1,5 @@
 import pytest
-import torch
+
 from transformers import BertTokenizer
 
 from hipool import curiam_reader
@@ -8,7 +8,7 @@ from hipool import curiam_reader
 @pytest.fixture
 def curiam_sample():
     bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
-    dataset = curiam_reader.CuriamDataset(
+    dataset = curiam_reader.DocDataset(
         json_file_path="fixtures/data/curiam_sample.json",
         tokenizer=bert_tokenizer,
         chunk_len=50,
@@ -24,9 +24,3 @@ def test_read_json_len(curiam_sample):
 def test_read_json_tokens(curiam_sample):
     assert curiam_sample.documents[0][0] == "Justice"
     assert curiam_sample.documents[0][1] == "GORSUCH"
-
-
-def test_getitem_labels(curiam_sample):
-    first_chunk_labels = curiam_sample[0]["targets"][0]
-    first_token_labels = first_chunk_labels[0]
-    assert torch.equal(first_token_labels, torch.zeros((9)))
